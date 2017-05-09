cccd34f2d5fd97485bb5a23c6edf8e3307463512
siriaodao <siriaodao@vip.qq.com>
2017-05-09 20:31:03 +0800

    update Dockerfile

--- build.sh
+++ build.sh
@@ -3,4 +3,3 @@ docker build -f Dockerfile.elasticsearch -t elasticsearch:5.3.0 ./
 docker build -f Dockerfile.logstash -t logstash:5.3.0 ./
 docker build -f Dockerfile.kibana -t kibana:5.3.0 ./
 docker build -f Dockerfile.filebeat -t filebeat:5.3.0 ./
-mkdir -p /opt/elk/data/elk/{elasticsearchdata,filebeatdata,kibanadata,logstashdata}
