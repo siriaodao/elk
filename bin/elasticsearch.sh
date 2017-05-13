2550511e79c512cd230c612fd2697ab6d0fc48d2
siriaodao <siriaodao@vip.qq.com>
2017-05-13 14:28:07 +0800

    Update elasticsearch.sh

--- bin/elasticsearch.sh
+++ bin/elasticsearch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "* soft nofile 65536" > /etc/security/limits.conf 
-echo "* hard nofile 131072" > /etc/security/limits.conf
-echo "* soft nproc 2048" > /etc/security/limits.conf
-echo "* hard nproc 4096" > /etc/security/limits.conf
-echo "vm.max_map_count=655360" > /etc/sysctl.conf 
+echo "* soft nofile 65536" >> /etc/security/limits.conf 
+echo "* hard nofile 131072" >> /etc/security/limits.conf
+echo "* soft nproc 2048" >> /etc/security/limits.conf
+echo "* hard nproc 4096" >> /etc/security/limits.conf
+echo "vm.max_map_count=655360" >> /etc/sysctl.conf 
 sysctl -p
 chown -R elk:elk /opt/product/elasticsearch
 chown -R elk:elk /opt/product/data/elk/elasticsearchdata
