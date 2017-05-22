ef31b54529113874d632457f5263d318a030da4b
siriaodao <siriaodao@vip.qq.com>
2017-05-23 01:38:40 +0800

    update docker-compose

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ docker build -f Dockerfile.logstash -t logstash:5.3.0 ./
 docker build -f Dockerfile.kibana -t kibana:5.3.0 ./
 docker build -f Dockerfile.filebeat -t filebeat:5.3.0 ./
 docker build -f Dockerfile.zkCluster -t zookeeper:3.4.9 ./
+docker build -f Dockerfile.kafkaCluster -t kafka:2.12 ./
