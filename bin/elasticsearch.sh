260dad433a468d7940e37bb9e3c80e872d1fa472
siriaodao <siriaodao@vip.qq.com>
2017-05-23 21:56:17 +0800

    update es bin

--- bin/elasticsearch.sh
+++ bin/elasticsearch.sh
@@ -7,7 +7,7 @@ echo "vm.max_map_count=655360" >> /etc/sysctl.conf
 sysctl -p
 ES_DATA_DIR="/opt/product/data/elk/elasticsearchdata"
 ES_CONF="/opt/product/elasticsearch/config/elasticsearch.yml"
-ES_CONF_TXT_CLUSTER="cluster.name: ${ES_CLUSTER_NAME}\nnode.name: ${ES_NODE_NAME}\ndiscovery.type: single-node\nnode.master: true\ndiscovery.zen.ping.unicast.hosts: [${ES_NODE_LIST}]"
+ES_CONF_TXT_CLUSTER="cluster.name: ${ES_CLUSTER_NAME}\nnode.name: ${ES_NODE_NAME}\ndiscovery.zen.minimum_master_nodes: ${ES_NODE_NUMBER}\ndiscovery.zen.ping.unicast.hosts: [${ES_NODE_LIST}]"
 ES_CONF_TXT_SINGLE="network.host: 0.0.0.0\nhttp.port: 9200\npath.data: ${ES_DATA_DIR}\nhttp.cors.enabled: true\nhttp.cors.allow-origin: \"*\"\nnode.data: true"
 if [ -n "${ES_CLUSTER_NAME}" ];then
   echo -e "${ES_CONF_TXT_CLUSTER}" > ${ES_CONF}
