37a618b15cdd4556ae2ef8a3eb1d9c5c5fadf804
siriaodao <siriaodao@vip.qq.com>
2017-05-24 00:16:53 +0800

    update kafka.sh

--- bin/kafka.sh
+++ bin/kafka.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-KAFKA_CONF="/opt/product/kafka/config/server.properties"
+KAFKA_HOME="/opt/product/kafka"
+KAFKA_CONF="${KAFKA_HOME}/config/server.properties"
 KAFKA_DATA="/opt/product/data/elk/kafkadata"
 KAFKA_CONF_TXT="broker.id=${KAFKA_ID}\nhost.name=${hostname}\nport=9092\nlog.dirs=${KAFKA_DATA}\nsocket.send.buffer.bytes=102400\nsocket.receive.buffer.bytes=102400\nlog.retention.hours=168\nmessage.max.byte=5242880\ndefault.replication.factor=2\nreplica.fetch.max.bytes=5242880\nlog.retention.check.interval.ms=300000\nlog.cleaner.enable=false\nzookeeper.connect=${ZK_CLUSTER}"
 echo -e ${KAFKA_CONF_TXT} > ${KAFKA_CONF}
